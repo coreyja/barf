9a657ea1eab190382a65bf4a961dbe9b30489f89
Brad Van Vugt <1531419+bvanvugt@users.noreply.github.com>
2023-07-05 22:02:02 +0000

    Init Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
+	const exampleURL = 'https://board.battlesnake.com?engine=<ENGINE_URL>&game=<GAME_ID>';
 </script>
 
-<section>
-	<h1>Homepage.</h1>
-</section>
+
+<div>
+	<p class="mb-2 font-bold">To display a game you need to specify engine and game parameters in the URL.</p>
+	<p>For example: {exampleURL}</p>
+</div>
 
 <style>
 </style>
