63917df97a021856e97796a1c7aa279db59afe08
Corey Alexander <coreyja@gmail.com>
2023-07-07 23:53:06 +0000

    Personal Opinion Refactors
    Just my personal typescript style. Instead of passing in the coords
    seperately, I'm passing in a single object but then destructuring the
    coords out of it. The benefit is that if you already have a `Point` you
    can pass the whole thing in instead of having to reconstruct it.
    
    The shorthand for creating an object based on the variables names as also
    comes in handy here, when you already have just the x and y values.

--- src/lib/components/Gameboard.svelte
+++ src/lib/components/Gameboard.svelte
@@ -32,15 +32,15 @@
 	$: svgWidth = CELL_SIZE * frame.width + CELL_SPACING * frame.width + CELL_SPACING;
 	$: svgHeight = CELL_SIZE * frame.height + CELL_SPACING * frame.height + CELL_SPACING;
 
-	function svgCirclePropsAtCoords(x: number, y: number) {
-		const rect = svgRectPropsAtCoords(x, y);
+	function svgCirclePropsAtPoint({ x, y }: Point) {
+		const rect = svgRectPropsAtPoint({ x, y });
 		return {
 			cx: rect.x + CELL_SIZE / 2,
 			cy: rect.y + CELL_SIZE / 2
 		};
 	}
 
-	function svgRectPropsAtCoords(x: number, y: number) {
+	function svgRectPropsAtPoint({ x, y }: Point) {
 		return {
 			x: CELL_SPACING + x * (CELL_SIZE + CELL_SPACING),
 			y: svgHeight - (y + 1) * (CELL_SIZE + CELL_SPACING),
@@ -52,7 +52,7 @@
 	function polylinePoints(snake: Snake) {
 		const points = [];
 		for (let i = 0; i < snake.body.length; i++) {
-			const { cx, cy } = svgCirclePropsAtCoords(snake.body[i].x, snake.body[i].y);
+			const { cx, cy } = svgCirclePropsAtPoint(snake.body[i]);
 			points.push(`${cx},${cy}`);
 		}
 		return points.join(' ');
@@ -63,7 +63,12 @@
 	<!-- Grid -->
 	{#each { length: frame.width } as _, x}
 		{#each { length: frame.height } as _, y}
-			<rect id={`grid-${x}-${y}`} class="grid" fill={CELL_COLOR} {...svgRectPropsAtCoords(x, y)} />
+			<rect
+				id={`grid-${x}-${y}`}
+				class="grid"
+				fill={CELL_COLOR}
+				{...svgRectPropsAtPoint({ x, y })}
+			/>
 		{/each}
 	{/each}
 	<!-- Snakes -->
@@ -84,7 +89,7 @@
 			class="hazard"
 			fill={HAZARD_COLOR}
 			fill-opacity={HAZARD_OPACITY}
-			{...svgRectPropsAtCoords(hazard.x, hazard.y)}
+			{...svgRectPropsAtPoint(hazard)}
 		/>
 	{/each}
 	<!-- Food -->
@@ -94,7 +99,7 @@
 			class="food"
 			r={FOOD_RADIUS}
 			fill={FOOD_COLOR}
-			{...svgCirclePropsAtCoords(food.x, food.y)}
+			{...svgCirclePropsAtPoint(food)}
 		/>
 	{/each}
 </svg>
