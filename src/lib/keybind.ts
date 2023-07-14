714ff69909b7e006704c816d0d14ecd576d4a22f
Brad Van Vugt <1531419+bvanvugt@users.noreply.github.com>
2023-07-14 21:38:12 +0000

    Prevent default keyboard inputs.

--- src/lib/keybind.ts
+++ src/lib/keybind.ts
@@ -7,7 +7,12 @@ type Options = {
 
 export function keybind(node, options: Options) {
     const mousetrap = new Mousestrap(window.document);
-    mousetrap.bind([options.key], options.f);
+    mousetrap.bind([options.key], () => {
+        options.f();
+
+        // Always prevent default behavior
+        return false;
+    });
 
     return {
         destroy() {
