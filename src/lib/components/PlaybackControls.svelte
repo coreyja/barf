7e3b79b9f7d3f7a210257b18adcf5b3fabfea09e
Brad Van Vugt <1531419+bvanvugt@users.noreply.github.com>
2023-07-14 21:49:37 +0000

    Disable frame controls during playback

--- src/lib/components/PlaybackControls.svelte
+++ src/lib/components/PlaybackControls.svelte
@@ -10,29 +10,46 @@
 
 	export let handlers: PlaybackHandlers;
 	export let state: PlaybackState;
+
+	$: disableDuringPlayback = state == PlaybackState.PLAYING;
 </script>
 
 <p class="text-center text-xl">
-	{#if state == PlaybackState.PAUSED}
-		<button class="mx-2" on:click={handlers.first}>
-			<IconFirst />
-		</button>
-		<button class="mx-2" on:click={handlers.prev}>
-			<IconPrev />
-		</button>
-		<button class="mx-2" on:click={handlers.play}>
-			<IconPlay />
-		</button>
-		<button class="mx-2" on:click={handlers.next}>
-			<IconNext />
-		</button>
-		<button class="mx-2" on:click={handlers.next}>
-			<IconLast />
-		</button>
-	{/if}
+	<button
+		class="mx-2 disabled:text-gray-300"
+		on:click={handlers.first}
+		disabled={disableDuringPlayback}
+	>
+		<IconFirst />
+	</button>
+	<button
+		class="mx-2 disabled:text-gray-300"
+		on:click={handlers.prev}
+		disabled={disableDuringPlayback}
+	>
+		<IconPrev />
+	</button>
 	{#if state == PlaybackState.PLAYING}
 		<button class="mx-2" on:click={handlers.pause}>
 			<IconPause />
 		</button>
+	{:else if state == PlaybackState.PAUSED}
+		<button class="mx-2" on:click={handlers.play}>
+			<IconPlay />
+		</button>
 	{/if}
+	<button
+		class="mx-2 disabled:text-gray-300"
+		on:click={handlers.next}
+		disabled={disableDuringPlayback}
+	>
+		<IconNext />
+	</button>
+	<button
+		class="mx-2 disabled:text-gray-300"
+		on:click={handlers.next}
+		disabled={disableDuringPlayback}
+	>
+		<IconLast />
+	</button>
 </p>
